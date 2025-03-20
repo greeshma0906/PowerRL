d2456b809c35abade7660e78342307dee54f9064
Snehitha <psnehitha3333@gmail.com>
2025-03-20 14:33:36 +0530

    Fixed main.py

--- main.py
+++ main.py
@@ -25,6 +25,6 @@ def run_rl_script(script_path):
     energy_used = monitor.get_energy_usage()
     print(f"Total Energy Consumed: {energy_used:.3f} kWh")
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     rl_script = "rl_scripts/sample_rl.py"  # Replace with actual script path
     run_rl_script(rl_script)
